aaacd298cd0ca40726c7566fbfda502d4f5d46d2
Julian Arce <117579382+anubiss10@users.noreply.github.com>
2024-07-19 15:37:36 +0200

    update domain

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -44,7 +44,7 @@ const Layout = ({ children, title, footer = true, dark = false }) => {
       {footer && (
         <footer className="bg-dark text-light text-center">
           <div className="container p-4">
-            <h1>&copy; Ryan Ray Portfolio</h1>
+            <h1>&copy; Julian Portfolio</h1>
             <p>2000 - {new Date().getFullYear()}</p>
             <p>All rights Reserved.</p>
           </div>
